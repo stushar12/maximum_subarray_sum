13a696116af9517a61e125dcd3f4c8778f47e15c
Tushar Saxena <stushar345@gmail.com>
2020-07-28 18:51:09 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+  int circular_sum(int a[],int n)
+  {
+    int total=0;
+    int a1=kadane(a,n);
+  for (int i=0;i<n;i++)
+  {
+    total=total+a[i];
+    a[i]=-a[i];
+  }
+  int circular_total=kadane(a,n);
+  total=total+circular_total;
+  return (total>a1) ? total:a1;
+  }
+
+
+  int kadane(int a[] , int n)
+  {
+  int sum=0,sum1=0;
+    for(int i=0;i<n;i++)
+      {
+         sum=sum+a[i];
+            if(sum<0)
+               sum=0;
+            if(sum1<sum)
+              sum1=sum;   
+      }
+  return sum1;
+  }
+  
+int main()
+{      
+  int total=0,x=0,sum1=0;
+  int arr[]={2,1,-5,4,-3,1,-3,1,-3,4,-1};
+  int n=sizeof(arr)/sizeof(arr[0]);
+  total=circular_sum(arr,n);
+  printf("Maximum sum is %d ",total);
+}
